a2f0a2003f3a63141494a46a0862337c6a8daaf8
steveokay <mokaysteve@gmail.com>
2020-07-23 16:29:58 +0300

    Adding Bean Validation. closes #7

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -15,18 +19,30 @@ import java.util.UUID;
 @AllArgsConstructor
 public class BeerDto {
 
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
 
+    @Null
     private OffsetDateTime createdDate; // we will be using UTC Date for our dates
+
+    @Null
     private  OffsetDateTime lastModifiedDate;
 
+    @NotBlank
     private String beerName;
 
+    @NotNull
     private BeerStyleEnum beerStyle;
 
+    @Positive
+    @NotNull
     private Long upc;
 
+    @NotNull
+    @Positive
     private BigDecimal price;
 
     private  Integer quantityOnHand;
